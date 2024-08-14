5614253fcb31387aee4320f976cde96002db1c15
Austin Norgaard <austinnorgaard@gmail.com>
2024-08-13 21:12:47 -0700

    fixed warnings

--- src/app.jsx
+++ src/app.jsx
@@ -9,34 +9,42 @@ import HomePage from './Components/Pages/HomePageComponent/HomePageComponent';
 function App() {
 
   let navigate = useNavigate();
+  // eslint-disable-next-line 
   const homeRoute = () => {
     let path = '/';
     navigate(path);
   }
+  // eslint-disable-next-line 
   const storyRoute = () => {
     let path = '/story';
     navigate(path);
   }
+  // eslint-disable-next-line 
   const photoRoute = () => {
     let path = '/photos';
     navigate(path);
   }
+  // eslint-disable-next-line 
   const weddingPartyRoute = () => {
     let path = '/wedding-party';
     navigate(path);
   }
+  // eslint-disable-next-line 
   const faqRoute = () => {
     let path = '/faq';
     navigate(path);
   }
+  // eslint-disable-next-line 
   const travelRoute = () => {
     let path = '/travel';
     navigate(path);
   }
+  // eslint-disable-next-line 
   const todoRoute = () => {
     let path = '/things-to-do';
     navigate(path);
   }
+  // eslint-disable-next-line 
   const registryRoute = () => {
     let path = '/registry';
     navigate(path);
