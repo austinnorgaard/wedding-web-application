1ce4b5002e436f5ffb15839671460bd5aeb6354b
Austin Norgaard <austinnorgaard@gmail.com>
2024-08-18 09:05:13 -0700

    added sort

--- src/Components/Utilities/RegistryGalleryComponent/RegistryGalleryComponent.jsx
+++ src/Components/Utilities/RegistryGalleryComponent/RegistryGalleryComponent.jsx
@@ -12,6 +12,7 @@ function RegistryGallery() {
   const [priceChecked, setPriceChecked] = useState (["", ""]);
   const [storeChecked, setStoreChecked] = useState ("");
   const [statusChecked, setStatusChecked] = useState ("");
+  const [load, setLoad] = useState ("unloaded");
   const priceOptions = [
     { label: "$0-49", value: ["0.00", "49.99"] },
     { label: "$50-99", value: ["50.00","99.99"] },
@@ -20,8 +21,8 @@ function RegistryGallery() {
   ];
   const storeOptions = [
     { label: "Amazon", value: "amazon" },
-    { label: "Serta", value: "serta" },
-    { label: "Ring", value: "ring" }
+    { label: "Serta", value: "Serta" },
+    { label: "Ring", value: "Ring" }
   ];
   const statusOptions = [
     { label: "Available", value: "available" },
@@ -30,7 +31,45 @@ function RegistryGallery() {
   const quantityNeeded = [
     { label: "serta", value: 1 },
     { label: "ring", value: 1 },
+    { label: "amazon", value: 1}
   ];
+  const [storeItems, setStoreItems] = useState ([
+    { 
+      storeName: "Serta",
+      productName: "Serta iComfortECO Foam Mattress", 
+      productUrl: "https://www.serta.com/products/icomforteco-foam-mattress?variant=44416523665572&irclickid=1vTVaR1g5xyKWK-Vd7WwnQt3UkCzJVQpMxZYzU0&irgwc=1&utm_campaign=Skimbit%20Ltd.&utm_source=impact&utm_medium=affliate&utm_content=Online%20Tracking%20Link",
+      imageUrl: "https://www.serta.com/cdn/shop/files/gzbe2putpha6vcmgtqu8_abf8b5e4-b0c4-44ca-a774-2e2b0380b62d.jpg?v=1697066047&width=2000",
+      productPrice: 3099.00,
+      qtyNeeded: quantityNeeded[0].value
+    },
+    {
+      storeName: "Ring",
+      productName: "Ring Wired Doorbell Pro",
+      productUrl: "https://ring.com/products/video-doorbell-pro-2",
+      imageUrl: "https://images.ctfassets.net/a3peezndovsu/variant-31961428492377/e8d3f08c98ee484eef46c383b85cb785/variant-31961428492377.jpg",
+      productPrice: 229.99,
+      qtyNeeded: quantityNeeded[1].value
+    }
+  ]);
+  const sortFilters = [
+    { label: "Featured", value: "feat" },
+    { label: "Price high to low", value: "htl" },
+    { label: "Price low to high", value: "lth" }
+  ]
+  const [filterType, setFilterType] = useState("feat");
+
+  function onFilterChange (value) {
+    setFilterType(value);
+    if (value === "feat") {
+      setStoreItems(storeItems);
+    }
+    else if (value === "htl") {
+      setStoreItems(storeItems.sort((a, b) => a.productPrice > b.productPrice ? -1.00 : 1.00));
+    }
+    else if (value === "lth") {
+      setStoreItems(storeItems.sort((a, b) => a.productPrice < b.productPrice ? -1.00 : 1.00));
+    }
+  }
 
 
   function onPriceChange (value) {
@@ -71,28 +110,28 @@ function RegistryGallery() {
     }
     setFilterMenuClicked (!filterMenuClick);
   }
-
-  function priceConversion (price) {
-    if (price !== null) {
-      let priceConversion = "";
-      priceConversion = price;
-      priceConversion = priceConversion.replace("$", ""); 
-      priceConversion = priceConversion.replace(",", ""); 
-      priceConversion = parseFloat(priceConversion); 
-      console.log(priceConversion);
-      return priceConversion;
-    }
-  }
  
   useEffect(() => {
     axios.get('https://weddingapi.norgaardfamily.com/https://www.amazon.com/wedding/items/2PMC8XDS4JY6F?page=1&filter=noFilter&sort=priority&direction=descending&prime=false', {id: 1})
   .then(function (response) {
     let maxItems = response.data.result.filteredItemTotal;
     for (let i = 0; i < maxItems; i++) {
-      setItems(response.data.result.minimalRegistryItems);
+      setItems(response.data.result.minimalRegistryItems); 
     }
-  });
-  }, []);
+  }).then (function (value) {
+    items.map((item, key) => (
+      storeItems.push({
+        storeName: "amazon", 
+        productName: item.productTitle, 
+        productUrl: "https://www.amazon.com" + item.productUrl, 
+        imageUrl: item.imageUrl, 
+        productPrice: item.itemPrice.amount, 
+        qtyNeeded: item.qtyNeeded})
+    ))
+    setLoad("loaded")})
+  }, [load]);
+
+  console.log(storeItems);
 
   return (
     <div className="RegistryGallery" id="content">
@@ -123,10 +162,10 @@ function RegistryGallery() {
             </div>
             <div className='RegistrySortBox'>
               <label for="sort">Sort by</label>
-              <select name="sort" id="sort">
-                <option value="featured">Featured</option>
-                <option value="pricehighlow">Price high to low</option>
-                <option value="pricelowhigh">Price low to high</option>
+              <select name="sort" id="sort" onChange={() => onFilterChange(document.querySelector('#sort').value)}>
+                {sortFilters.map((filter, key) => (
+                  <option value={filter.value}>{filter.label}</option>
+                ))}
               </select>
               <img src={downarrow} alt="arrowdown"/>
             </div>
@@ -162,31 +201,13 @@ function RegistryGallery() {
               </div>
             </div>
             <div className="RegistryWishlistItems" id="registryItems">
-              {((storeChecked === "serta" || storeChecked === "") && ((parseFloat(priceChecked[0]) <= 3099 && parseFloat(priceChecked[1]) >= 3099) || (priceChecked[0] === "" && priceChecked[1] === "")) && ((statusChecked === 'available' && quantityNeeded[0].value > 0) || (statusChecked === "purchased" && quantityNeeded[0].value === 0) || (statusChecked === ""))) &&
-              <Link to="https://www.serta.com/products/icomforteco-foam-mattress?variant=44416523665572&irclickid=1vTVaR1g5xyKWK-Vd7WwnQt3UkCzJVQpMxZYzU0&irgwc=1&utm_campaign=Skimbit%20Ltd.&utm_source=impact&utm_medium=affliate&utm_content=Online%20Tracking%20Link" target='_blank' id="item">
-                <img src="https://www.serta.com/cdn/shop/files/gzbe2putpha6vcmgtqu8_abf8b5e4-b0c4-44ca-a774-2e2b0380b62d.jpg?v=1697066047&width=2000" alt="serta bed"/>
-                <div className="ItemTextContainer" id="sertaItemText">
-                  <h4 id="itemtitle">Serta iComfortECO Foam Mattress</h4>
-                  <h4 id="itemprice">$3099.00</h4>
-                </div>
-              </Link>
-              }
-              {((storeChecked === "ring" || storeChecked === "") && ((parseFloat(priceChecked[0]) <= 229.99 && parseFloat(priceChecked[1]) >= 229.99) || (priceChecked[0] === "" && priceChecked[1] === "")) && ((statusChecked === 'available' && quantityNeeded[1].value > 0) || (statusChecked === "purchased" && quantityNeeded[1].value === 0) || (statusChecked === ""))) &&
-              <Link to="https://ring.com/products/video-doorbell-pro-2" target='_blank' id="item">
-                <img src="https://images.ctfassets.net/a3peezndovsu/variant-31961428492377/e8d3f08c98ee484eef46c383b85cb785/variant-31961428492377.jpg" alt="Ring Camera"/>
-                <div className="ItemTextContainer" id="ringItemText">
-                  <h4 id="itemtitle">Ring Wired Doorbell Pro</h4>
-                  <h4 id="itemprice">$229.99</h4>
-                </div>
-              </Link>
-              }
-              {items.map((item, key) => (
-                ((storeChecked === "amazon" || storeChecked === "") && ((parseFloat(priceChecked[0]) <= priceConversion(item.itemPrice.displayString) && parseFloat(priceChecked[1]) >= priceConversion(item.itemPrice.displayString)) || (priceChecked[0] === "" && priceChecked[1] === "")) && ((statusChecked === 'available' && item.qtyNeeded > 0) || (statusChecked === "purchased" && item.qtyNeeded === 0) || (statusChecked === ""))) &&
-                <Link to={"https://www.amazon.com" + item.productUrl} target='_blank' id="item" index={key}>
-                  <img src={item.imageUrl} alt={item.productTitle}/>
-                  <div className="ItemTextContainer" id="amazonItemText">
-                    <h4 id="itemtitle">{item.productTitle}</h4>
-                    <h4 id="itemprice">{item.itemPrice.displayString}</h4>
+              {storeItems.map((store, key) => (
+                ((storeChecked === store.storeName || storeChecked === "") && ((parseFloat(priceChecked[0]) <= store.productPrice && parseFloat(priceChecked[1]) >= store.productPrice) || (priceChecked[0] === "" && priceChecked[1] === "")) && ((statusChecked === 'available' && store.qtyNeeded > 0) || (statusChecked === "purchased" && store.qtyNeeded === 0) || (statusChecked === ""))) &&
+                <Link to={store.productUrl} target='_blank' id="item">
+                  <img src={store.imageUrl} alt={store.productName}/>
+                  <div className="ItemTextContainer" id={store.storeName+"ItemText"}>
+                    <h4 id="itemtitle">{store.productName}</h4>
+                    <h4 id="itemprice">${store.productPrice.toFixed(2)}</h4>
                   </div>
                 </Link>
               ))}
