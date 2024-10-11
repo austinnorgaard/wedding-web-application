87451431dc8c8b5b932bb92b819f980e755020e0
Austin Norgaard <me@austinnorgaard.com>
2024-10-11 16:22:49 -0700

    changed some hotel image urls and made the travel page mobile friendly

--- src/Components/Utilities/TravelListComponent/TravelListComponent.jsx
+++ src/Components/Utilities/TravelListComponent/TravelListComponent.jsx
@@ -7,12 +7,40 @@ import hotelpin from '../../../hotelpin.png'
 import hotels from '../Leaflet/hotels.json'
 
 function TravelList() {
+    const [locations, setLocations] = useState([])
+
+    hotels.map((hotel, id) => (
+        locations.push(hotel)
+    ))
 
     return (
         <div className="TravelList">
         
             <div className="MainContainer" id="TravelListMainContainer">
-                
+                {locations.map((location, id) => (
+                    <div className='TravelListLocations'>
+                        <div className='TravelListLocation'>
+                            <div className='TravelListLocationImage'>
+                                <img src={location.image_url} alt={location.name}/>
+                            </div>
+                            <div className='TravelListLocationRight'>
+                                <div className='TravelListLocationInfoContainer'>
+                                    <p className='TravelListLocationName'>{location.name}</p>
+                                    <div className='TravelListLocationRatingContainer'>
+                                        <p className='TravelListLocationRatingNum'>{location.rating} Stars - {location.price}</p>
+                                    </div>
+                                    <div className='TravelListLocationHeroContainer'>
+                                        <p className='TravelListLocationType'>{location.city} - </p>
+                                        <img src={markerImg} alt="" className='TravelListLocationHeroItem'/>
+                                    </div>
+                                </div>
+                                <div className='TravelListLocationButtonContainer'>
+                                    <Link to={'https://maps.apple.com/?q='+location.name+','+location.city+','+location.state} target='_blank' className='TravelListLocationButton'>Directions</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
