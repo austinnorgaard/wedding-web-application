7e893bdd73c77a013e76ec40edc2cdd90d9fd903
Austin Norgaard <austinnorgaard@gmail.com>
2024-10-18 22:49:54 -0700

    added locations and finished story page

--- src/Components/Utilities/TravelMapComponent/TravelMapComponent.jsx
+++ src/Components/Utilities/TravelMapComponent/TravelMapComponent.jsx
@@ -4,12 +4,16 @@ import './TravelMapComponent.scss';
 import postlewaitImg from "../../../Resources/Photos/postlewaits.png"
 import markerImg from '../../../Resources/Photos/marker.png'
 import hotelpin from '../../../Resources/Photos/hotelpin.png'
+import foodpin from '../../../Resources/Photos/foodpin.png'
+import funpin from '../../../Resources/Photos/funpoint.png'
 import lockedLock from '../../../Resources/Photos/lockedLock.png'
 import unlockedLock from '../../../Resources/Photos/unlockedLock.png'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import L from "leaflet"
 import osm from '../Leaflet/osm-providers'
 import hotels from '../Leaflet/hotels.json'
+import restaurants from '../Leaflet/food.json'
+import fun from '../Leaflet/thingsToDo.json'
 import "leaflet/dist/leaflet.css"
 
 function TravelMap() {
@@ -25,6 +29,18 @@ function TravelMap() {
     })
     const hotelIcon = new L.icon ({
         iconUrl: hotelpin,
+        iconSize: [35, 35],
+        iconAnchor: [17, 36],
+        popupAnchor: [0, -36]
+    });
+    const foodIcon = new L.icon ({
+        iconUrl: foodpin,
+        iconSize: [35, 35],
+        iconAnchor: [17, 36],
+        popupAnchor: [0, -36]
+    });
+    const funIcon = new L.icon ({
+        iconUrl: funpin,
         iconSize: [35, 35],
         iconAnchor: [17, 36],
         popupAnchor: [0, -36]
@@ -92,7 +108,7 @@ function TravelMap() {
                             </div>
                         </Popup>
                         {hotels.map((hotel, id) => (
-                            <Marker position={[hotel.lat, hotel.lng]} icon={hotelIcon} key={id}>
+                            <Marker position={[hotel.lat, hotel.lng]} icon={hotelIcon} key={id} id={hotel.name}>
                                 <Popup closeButton={false} className={hotel.name}>
                                 <div className='LocationImageContainer'>
                                     <img src={hotel.image_url} alt={hotel.name} className='LocationImage'/>
@@ -110,6 +126,54 @@ function TravelMap() {
                                     </div>
                                     <div className='LocationButtonContainer'>
                                         <Link to={'https://maps.apple.com/?q='+hotel.name+','+hotel.city+','+hotel.state} target='_blank' className='LocationButton'>Directions</Link>
+                                    </div>
+                                </div>
+                            </Popup>
+                        </Marker>
+                        ))}
+                        {restaurants.map((restaurant, id) => (
+                            <Marker position={[restaurant.lat, restaurant.lng]} icon={foodIcon} key={id} id={restaurant.name}>
+                                <Popup closeButton={false} className={restaurant.name}>
+                                <div className='LocationImageContainer'>
+                                    <img src={restaurant.image_url} alt={restaurant.name} className='LocationImage'/>
+                                </div>
+                                <div className='LocationContainer'>
+                                    <div className='LocationInfoContainer'>
+                                        <p className='LocationName'>{restaurant.name}</p>
+                                        <div className='LocationRatingContainer'>
+                                            <p className='LocationRatingNum'>{restaurant.rating} Stars - {restaurant.price}</p>
+                                        </div>
+                                        <div className='LocationHeroContainer'>
+                                            <p className='LocationType'>{restaurant.city} - </p>
+                                            <img src={markerImg} alt="" className='LocationHeroItem'/>
+                                        </div>
+                                    </div>
+                                    <div className='LocationButtonContainer'>
+                                        <Link to={'https://maps.apple.com/?q='+restaurant.name+','+restaurant.city+','+restaurant.state} target='_blank' className='LocationButton'>Directions</Link>
+                                    </div>
+                                </div>
+                            </Popup>
+                        </Marker>
+                        ))}
+                        {fun.map((toDo, id) => (
+                            <Marker position={[toDo.lat, toDo.lng]} icon={funIcon} key={id} id={toDo.name}>
+                                <Popup closeButton={false} className={toDo.name}>
+                                <div className='LocationImageContainer'>
+                                    <img src={toDo.image_url} alt={toDo.name} className='LocationImage'/>
+                                </div>
+                                <div className='LocationContainer'>
+                                    <div className='LocationInfoContainer'>
+                                        <p className='LocationName'>{toDo.name}</p>
+                                        <div className='LocationRatingContainer'>
+                                            <p className='LocationRatingNum'>{toDo.rating} Stars - {toDo.price}</p>
+                                        </div>
+                                        <div className='LocationHeroContainer'>
+                                            <p className='LocationType'>{toDo.city} - </p>
+                                            <img src={markerImg} alt="" className='LocationHeroItem'/>
+                                        </div>
+                                    </div>
+                                    <div className='LocationButtonContainer'>
+                                        <Link to={'https://maps.apple.com/?q='+toDo.name+','+toDo.city+','+toDo.state} target='_blank' className='LocationButton'>Directions</Link>
                                     </div>
                                 </div>
                             </Popup>
