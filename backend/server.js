270b586a59d9e2377e0ad31f27085332922b84e4
Austin Norgaard <austinnorgaard@gmail.com>
2024-10-22 00:18:02 -0700

    updated gitignore and beginning backend api

--- backend/server.js
+++ backend/server.js
@@ -2,45 +2,5 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors);
-// This is your test secret API key.
-const stripe = require("stripe")('sk_test_51PnZAoGobvn5ullRH4OXDL5Lowmj46lzeGlF5nBYTFL2a6OsR2NM2aKEPruA2vCNueIsFf1hIE04sgdmLR8uBMVs00zifkRXiL');
-
-app.use(express.static("public"));
-app.use(express.json());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
-const calculateOrderAmount = (items) => {
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
-  let total = 0;
-  items.forEach((item) => {
-    total += item.amount;
-  });
-  return total;
-};
-
-app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
-
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: "usd",
-    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
-
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
-
 
 app.listen(4242, () => console.log("Node server listening on port 4242!"));
