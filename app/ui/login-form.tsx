840faf368980ff2c7990d36f34f1e7521a9ca22b
Austin Norgaard <me@austinnorgaard.com>
2024-12-05 01:13:15 -0800

    removed localstorage file

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useActionState, useState } from 'react';
+import { useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
 import {
   ExclamationCircleIcon,
